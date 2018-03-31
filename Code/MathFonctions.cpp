785a17d7ba3f50cdc76863d0d8dafd519997ac3c
TeddyBigBear <theodore.bories@free.fr>
2018-03-31 08:45:35 +0200

    Ajout fichier MathFonctions.cpp/.hpp
    Creation branche Fonction principales avec fichier Maths fonction vides

---
+++ Code/MathFonctions.cpp
@@ -0,0 +1,9 @@
+/* 
+MathFonction.cpp
+Projet Calculatrice / Groupe : Sonia 
+Date de creation : 31/03/2018
+*/
+
+
+#include "MathFonction.hpp"
+
---
+++ Code/MathFonctions.hpp
@@ -0,0 +1,7 @@
+/* 
+MathFonction.hpp
+Projet Calculatrice / Groupe : Sonia 
+Date de creation : 31/03/2018
+*/
+
+
