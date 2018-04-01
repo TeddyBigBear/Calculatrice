e44054c0b34b9bbd9bf58357425526c192e5c5dd
TeddyBigBear <theodore.bories@free.fr>
2018-04-01 12:49:43 +0200

    Ajout fonctions basiques mathematiques
    Ajout  +, -, /, x en methodes privée pour ne pas avoir a instancier d'objet lors de l'appel de ces methodes

--- Code/MathFonctions.cpp
+++ Code/MathFonctions.cpp
@@ -7,3 +7,27 @@ Date de creation : 31/03/2018
 
 #include "MathFonctions.hpp"
 
+using namespace std;
+
+namespace MathFonction
+{
+    double MathFonction::Additioner(double a, double b)
+    {
+        return a + b;
+    }
+
+    double MathFonction::Soustraire(double a, double b)
+    {
+        return a - b;
+    }
+
+    double MathFonction::Multiplier(double a, double b)
+    {
+        return a * b;
+    }
+
+    double MathFonction::Diviser(double a, double b)
+    {
+        return a / b;
+    }
+}
--- Code/MathFonctions.hpp
+++ Code/MathFonctions.hpp
@@ -5,3 +5,26 @@ Date de creation : 31/03/2018
 */
 
 
+/* 
+Pour respecter le paradigme de rogramation orientée objet on crée une classe meme si les methodes sont basiques.
+Mais toutes les methodes sont static pour ne pas avoir a instancier l'objet.
+Les fonction s'appelent comme ca : resultat = MathFonction::Additioner(a, b);
+*/
+namespace MathFonction
+{
+    class MathFonction
+    {
+    public:
+        // Returns a + b
+        static double Additioner(double a, double b);
+
+        // Returns a - b
+        static double Soustraire(double a, double b);
+
+        // Returns a * b
+        static double Multiplier(double a, double b);
+
+        // Returns a / b
+        static double Diviser(double a, double b);
+    };
+} 
