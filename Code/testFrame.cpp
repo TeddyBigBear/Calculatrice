e59786c93c0d97156a18f371d061fdc233ce3c58
soniajore <sonia.jore83@orange.fr>
2018-04-17 21:50:19 +0200

    Ajout d'un test case pour la frame
    Il faut mettre à jour le makefile

---
+++ Code/testFrame.cpp
@@ -0,0 +1,18 @@
+/* 
+ * Projet POO - Calculatrice
+ * BORIES - JORE - MARTINEZ - PHILIP
+ */
+
+//#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include <iostream>
+#include "catch.hpp"
+#include "framePrincipale.hpp"
+
+using namespace std;
+
+TEST_CASE("Test frame principale", "[framePrincipale]") {
+
+    CHECK(2==2);
+
+}
+
